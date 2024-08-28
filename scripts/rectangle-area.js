ec9e43727d4c9531721eb4bbea343c17aedb44ee
mizanff <mrmizanurrahman626@gmail.com>
2024-08-28 12:16:44 +0600

    create a rectangle area

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,22 @@
 function calculateRectangleArea(){
-    console.log(calculateRectangleArea);
+    // get length of the rectangle 
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // get with of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    // calculate rectangle area
+    const area = length * width;
+    console.log('Area of the length', area)
+
+    // display rectangle
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+   
 }
