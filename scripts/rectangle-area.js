c5f2562d2f78d70d2c2d82121e56bc4d8d1f6316
mizanff <mrmizanurrahman626@gmail.com>
2024-08-28 12:59:19 +0600

    added a parallogram area

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -20,3 +20,22 @@ function calculateRectangleArea(){
     rectangleAreaSpan.innerText = area;
    
 }
+
+// parallelogram 
+function calculateParallelogramArea(){
+    const baseInput = document.getElementById('parallelogram-base');
+    const baseText = baseInput.value;
+    const base = parseFloat(baseText);
+    console.log(base);
+
+    const heightInput = document.getElementById('parallelogram-height');
+    const heightText = heightInput.value;
+    const height = parseFloat(heightText);
+    console.log(height);
+
+    const area = base * height;
+    console.log(area);
+
+    const parallelogramAreaSpan = document.getElementById('parallelogram-area');
+    parallelogramAreaSpan.innerText = area;
+}
